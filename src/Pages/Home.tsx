c496378e8db9914521717057c60090ac858a2f74
isakli05 <isakaya709@gmail.com>
2023-01-13 21:08:59 +0300

    meta viewport set

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import Free from '../components/Home/Free'
 import Popular from '../components/Home/Popular'
 import Trending from '../components/Home/Trending'
 
 function Home() {
+
   return (
     <div className='bg-slate-900'>
       <Popular/>
